43c92a82786b39a040cd7d0fee66cc70e8b37e88
Joshua Stover <josh@jstover.net>
2022-07-10 18:00:50 +1000

    Remove unused code / dump environment in action

--- .github/actions/docker-compose-deploy/entrypoint.sh
+++ .github/actions/docker-compose-deploy/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eux
 
+set
+
 ERROR=0
 
 missing_parameter(){
@@ -20,14 +22,6 @@ if [ -z "$DOCKER_PASSWORD" ]; then missing_parameter docker_password ; fi
 
 if [ $ERROR -eq 1 ]; then exit 1; fi
 
-#mkdir -m 700 ~/.ssh
-#cat << EOF > "$HOME/.ssh/config"
-#Host *
-#  StrictHostKeyChecking no
-#  ControlMaster auto
-#  ControlPath ~/.ssh/master-%r@%h:%p
-#EOF
-
 eval "$(ssh-agent)"
 echo "$SSH_KEY" | tr -d '\r' | ssh-add -
 ssh-add -L
@@ -40,6 +34,4 @@ if [ -n "$DOCKER_USERNAME" ] && [ -n "$DOCKER_PASSWORD" ]; then
     echo "$DOCKER_PASSWORD" | docker login "$DOCKER_REGISTRY" -u "$DOCKER_USERNAME" --password-stdin
 fi;
 
-docker info
-docker run hello-world
 docker stack deploy --compose-file "${STACK_FILE}" --with-registry-auth "${STACK_NAME}"
