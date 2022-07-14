42ae3c34a0ed2da9beb88f039abbdc5928de1112
Joshua Stover <josh@jstover.net>
2022-07-14 19:59:27 +1000

    Update github secrets

--- terraform/github.tf
+++ terraform/github.tf
@@ -1,17 +1,17 @@
 resource "github_actions_secret" "deployer_ssh_private_key" {
   repository      = var.github_repository
   secret_name     = "DEPLOYER_SSH_PRIVATE_KEY"
-  plaintext_value = tls_private_key.deployer.private_key_pem
+  plaintext_value = tls_private_key.deployer.private_key_openssh
 }
 
 resource "github_actions_secret" "deployer_username" {
   repository      = var.github_repository
   secret_name     = "DEPLOYER_USERNAME"
-  plaintext_value = tls_private_key.deployer.private_key_pem
+  plaintext_value = var.deployer_username
 }
 
 resource "github_actions_secret" "primary_instance_ip" {
   repository      = var.github_repository
   secret_name     = "PRIMARY_INSTANCE_IP"
-  plaintext_value = tls_private_key.deployer.private_key_pem
+  plaintext_value = data.oci_core_vnic.app_vnic["primary"].public_ip_address
 }
